f32c5a4c63086de68da968bf19326f6dd5046c2c
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-10-25 16:51:22 +0200

    test: :white_check_mark:Adds additional test to test node ID retrieval from running OPC UA server (has to be started using a separate Maven profile)

--- lib/src/test/java/plc2skill/test/MappingTest.java
+++ lib/src/test/java/plc2skill/test/MappingTest.java
@@ -1,6 +1,8 @@
 package plc2skill.test;
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -16,7 +18,7 @@ class MappingTest {
 
 
 	@Test
-	void testMapping() throws IOException {
+	void testMappingWithoutPlc() throws IOException {
 		Path mappingPath = Paths.get("src", "test", "resources", "Test-PlcOpenXml.xml").toAbsolutePath();
 		String endpointUrl = "opc.tcp://localhost:4840";
 		String nodeIdRoot = "|var|CODESYS Control Win V3 x64";
@@ -26,6 +28,32 @@ class MappingTest {
 		
 		String actualResult = mapper.executeMapping();
 		byte[] fileFromBytes = Files.readAllBytes(Paths.get("src", "test", "resources", "ExpectedMappingOutput.ttl"));
+		String expectedResult = new String(fileFromBytes, StandardCharsets.UTF_8);
+		
+		assertThat(actualResult).isEqualToIgnoringWhitespace(expectedResult);
+	}
+	
+	
+	/**
+	 * A test that maps an PLCopenXML and gets information from a running PLC.
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 */
+	@Test
+	void testMappingWithPlc() throws IOException {
+		Path plcOpenXmlPath = Paths.get("src", "test", "resources", "Test-PlcOpenXml.xml").toAbsolutePath();
+		String endpointUrl = "opc.tcp://127.0.0.1:4840";
+		Plc2SkillMapper mapper = new Plc2SkillMapper.Builder(plcOpenXmlPath, endpointUrl)
+				.setUser("admin", "admin")
+				.build();
+		
+		String actualResult = mapper.executeMapping();
+		byte[] fileFromBytes = Files.readAllBytes(Paths.get("src", "test", "resources", "ExpectedMappingOutput_PLC.ttl"));
+		Path outputPath = Paths.get("src", "test", "resources", "out.ttl");
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath.toString()));
+	    writer.write(actualResult);
+	    
+	    writer.close();
 		String expectedResult = new String(fileFromBytes, StandardCharsets.UTF_8);
 		
 		assertThat(actualResult).isEqualToIgnoringWhitespace(expectedResult);
